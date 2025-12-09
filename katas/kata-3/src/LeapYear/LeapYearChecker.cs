59b67357e216c823ff2002e6d7540513bdbc857b
Jaime Forero <jaime.forero@sinco.com.co>
2025-12-09 11:45:01 -0500

    🔴🟢 Year_NotDivisibleBy4_IsNotLeapYear

--- katas/kata-3/src/LeapYear/LeapYearChecker.cs
+++ katas/kata-3/src/LeapYear/LeapYearChecker.cs
@@ -4,8 +4,9 @@ namespace LeapYear
     {
         public static bool IsLeapYear(int year)
         {
-            // TODO: Implement leap year logic using TDD
-            throw new NotImplementedException("Implement using TDD: Red → Green → Refactor");
+            if (year % 4 == 0)
+                return true;
+            return false;
         }
     }
 }
--- katas/kata-3/tests/LeapYear.Tests/LeapYearCheckerTests.cs
+++ katas/kata-3/tests/LeapYear.Tests/LeapYearCheckerTests.cs
@@ -4,12 +4,12 @@ namespace LeapYear.Tests
 {
     public class LeapYearCheckerTests
     {
-        [Fact]
-        public void Year_DivisibleBy4_ButNotBy100_IsLeapYear()
-        {
-            // Ejemplo: 2004 es año bisiesto
-            Assert.True(LeapYearChecker.IsLeapYear(2004));
-        }
+        // [Fact]
+        // public void Year_DivisibleBy4_ButNotBy100_IsLeapYear()
+        // {
+        //     // Ejemplo: 2004 es año bisiesto
+        //     Assert.True(LeapYearChecker.IsLeapYear(2004));
+        // }
 
         [Fact]
         public void Year_NotDivisibleBy4_IsNotLeapYear()
