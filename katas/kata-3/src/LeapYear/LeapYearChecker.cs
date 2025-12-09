5c90e120a629b980a556d20d7b02b286bd05f5c4
Jaime Forero <jaime.forero@sinco.com.co>
2025-12-09 12:29:05 -0500

    🟩 Year_IsMayor4000_throwExceptionn

--- katas/kata-3/src/LeapYear/LeapYearChecker.cs
+++ katas/kata-3/src/LeapYear/LeapYearChecker.cs
@@ -6,6 +6,9 @@ namespace LeapYear
         {
             if (year < 0)
                 throw new ArgumentOutOfRangeException(nameof(year), "Year cannot be negative"); 
+            if (year >= 4000)
+                throw new ArgumentOutOfRangeException(nameof(year), "Year cannot be mayor that 4000 years");
+            
             if (year % 100 == 0 && year % 400 != 0)
                 return false; 
             if (year % 4 == 0 )
